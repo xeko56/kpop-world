ab60a736fac744ebc5430f0b4bda101321c60dde
Quoc Anh Luu <markarkus06@gmail.com>
2023-03-24 00:39:37 +0100

    navigation in menubar

--- server/routes/Cards.js
+++ server/routes/Cards.js
@@ -6,17 +6,25 @@ const router = Router();
 router.get('/', async (_req, res) => {
     try {
         let conn = await pool.getConnection();
-        let sql = `SELECT card_nr, card_name, era_name, group_name, type_name, release_date, img_url FROM cards
+        let sql = '';
+        sql = `SELECT card_nr, card_name, era_name, group_name, type_name, release_date, img_url FROM cards
                         LEFT JOIN groups USING (group_nr)
                         LEFT JOIN eras USING (era_nr)
-                        LEFT JOIN types USING (type_nr)`;
+                        LEFT JOIN types USING (type_nr)`;      
+        if(_req.query.group_nr) {
+          sql = `SELECT card_nr, card_name, era_name, group_name, type_name, release_date, img_url FROM cards
+                        LEFT JOIN groups USING (group_nr)
+                        LEFT JOIN eras USING (era_nr)
+                        LEFT JOIN types USING (type_nr)
+                      WHERE group_nr = ${_req.query.group_nr}`;
+        }
         const rows = await conn.query(sql);
         conn.end();
         res.status(200).json({ data: rows });
         return rows;
       } catch (err) {
-        res.status(500).json({ message: 'Cannot get card list. Please try again later.' });
-        throw err;
+          res.status(500).json({ message: 'Cannot get card list. Please try again later.' });
+          throw err;
       }
 });
 
@@ -34,8 +42,8 @@ router.get('/:card_nr', async (req, res) => {
     res.status(200).json({ data: rows });
     return rows;
   } catch (err) {
-    res.status(500).json({ message: 'Cannot get card info. Please try again later.' });
-    throw err;
+      res.status(500).json({ message: 'Cannot get card info. Please try again later.' });
+      throw err;
   } 
 });
 
